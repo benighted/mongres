139f58eab7ecec62528c727e41e55751909c2955
Michael Payne <benighted@gmail.com>
2013-11-22 17:37:43 -0500

    Allow array of operations. Added error if no db port defined. Added debug mode switches (-d, -db, --debug) Added separate script for CLI operation (mongres.js)

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-var path = require('path');
+var util = require('util');
+var async = require('async');
 
 var Database = require('./lib/Database');
 var Operation = require('./lib/Operation');
 
-var config = null;
-var debugMode = false;
+var Mongres = function Mongres(config) {
+  // wrap op in array for compatibility
+  if (!util.isArray(config.op)) {
+    config.op = [config.op];
+  }
 
-var Mongres = module.exports = function Mongres(config) {
-	var op = new Operation(config);
-	op.run(function (err) {
-	  if (err) console.error(err);
-	  op.finish(function (err) {
-	    if (err) console.error(err);
-	    console.log('Operation finished.');
-	  });
-	});
+  this.config = config;
 };
 
-var configs = [];
+Mongres.prototype.run = function () {
+  var queue = [];
 
-for (var i = 2; i < process.argv.length; i++) {
-  switch (process.argv[i]) {
-    case "-d":
-    case "--debug":
-      debugMode = true;
-      break;
-    default:
-      config = require(path.resolve(process.argv[i]));
-      break;
+  // queue defined operations
+  for (var i in this.config.op) {
+    queue.push(new Operation({
+      db: this.config.db,
+      op: this.config.op[i],
+      debug: this.config.debug || this.config.op.debug
+    }));
   }
-}
 
-if (config) new Mongres(config);
+  // run operations in series
+  console.info('Running operations...');
+  async.eachSeries(queue, function (op, next) {
+    op.run(function (err) {
+      if (err) console.error(err);
+      op.finish(next);
+    });
+  }, function (err) {
+    if (err) return console.error(err);
+    console.info('All operations have finished.');
+  });
+};
+
+module.exports = Mongres;
