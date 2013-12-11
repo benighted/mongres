043939f8ff1dee8e2fc1cf938ef20f2b7b8c59ca
Michael Payne <benighted@gmail.com>
2013-12-11 09:59:13 -0500

    Use Mongres class from lib

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-var util = require('util');
-var async = require('async');
-
-var Database = require('./lib/Database');
-var Operation = require('./lib/Operation');
-
-var Mongres = function Mongres(config) {
-  // wrap op in array for compatibility
-  if (!util.isArray(config.op)) {
-    config.op = [config.op];
-  }
-
-  this.config = config;
-};
-
-Mongres.prototype.run = function (callback) {
-  var queue = [];
-
-  // queue defined operations
-  for (var i in this.config.op) {
-    queue.push(new Operation({
-      db: this.config.db,
-      op: this.config.op[i],
-      debug: this.config.debug || this.config.op.debug
-    }));
-  }
-
-  // run operations in series
-  async.eachSeries(queue, function (op, next) {
-    op.run(function (err) {
-      op.finish(next.bind(this, err));
-    });
-  }, callback);
-};
+var Mongres = require('./lib/Mongres');
 
 module.exports = Mongres;
