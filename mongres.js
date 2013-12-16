154f1e6fff86149b26f751073f7b68fcf8503182
Michael Payne <benighted@gmail.com>
2013-12-16 12:56:15 -0500

    Better file handling and config monitoring

--- mongres.js
+++ mongres.js
@@ -1,42 +1,58 @@
+var fs = require('fs');
 var path = require('path');
 var async = require('async');
 
 var Mongres = require('./index');
 
-var configs = [];
+var configPaths = [];
 var period = null;
 var debug = false;
 
 // process cli arguments
 for (var i = 2; i < process.argv.length; i++) {
   switch (process.argv[i]) {
+    // debugging options
     case "-d":
     case "-db": // works with nodemon
     case "--debug":
       debug = true;
       break;
+
+    // time period between executions
     case "-p":
     case "--period":
       if (period) throw new Error('period specified more than once');
       period = parseInt(process.argv[++i], 10);
       break;
+
+    // input config file(s) specification
     case "-f":
     case "--file":
-      configs.push(require(path.resolve(process.argv[++i])));
-      break;
-    default: // assume file path by default
-      configs.push(require(path.resolve(process.argv[i])));
+      ++i; // use next argument for file path
+    default: // assume a file path by default
+      try {
+        var cPath = path.resolve(process.argv[i]);
+        var cIndex = configPaths.length;
+
+        configPaths.push(cPath);
+        fs.watchFile(cPath, function (prev, curr) {
+          console.log('Detected change to ' + cPath);
+          delete require.cache[require.resolve(cPath)];
+        });
+      } catch (err) {
+        throw new Error('invalid module specified: ' + process.argv[i]);
+      }
       break;
   }
 }
 
 (function runConfigs() {
   var start = new Date();
-
   // run config sets in parallel
-  async.each(configs, function (config, next) {
-    if (debug) config.debug = debug;
-    new Mongres(config).run(next);
+  async.each(configPaths, function (cPath, next) {
+    var cModule = require(cPath);
+    if (debug) cModule.debug = debug;
+    new Mongres(cModule).run(next);
   }, function (err) {
     if (err) {
       console.error(err);
