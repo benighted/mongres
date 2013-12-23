aa0b25876c5ece27d874649a49426cd3fcd527ef
Michael Payne <benighted@gmail.com>
2013-12-23 11:08:35 -0500

    Updated test, moved to /tests folder

---
+++ tests/db.sample.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mongo: {
+    type: "mongodb",
+    name: "test",
+    host: "localhost",
+    port: 27017,
+    user: "",
+    pass: ""
+  },
+  postgres: {
+    type: "postgresql",
+    name: "test",
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    pass: ""
+  }
+};
--- tests/test.js
+++ tests/test.js
@@ -1,104 +1,8 @@
-# Mongres ETL System for Node.js
-### Synchronize PostgreSQL and MongoDB using Node.js
+var config = module.exports = {
+  debug: 1,
+  verbose: 1,
 
-Mongres uses configurable modules containing database and operation definitions to perform arbitrary ETL _(extract, transform, load)_ procedures.  This project is still in development and so core fundamentals could be changed without notice.  Contributions, comments, and issue reports are __always__ welcome.
-
-
-### Installation
-
-    npm install mongres
-
-
-### Command Line Usage
-
-    node mongres <path to module file>
-
-
-### Database Definitions
-
-Databases can be defined and used almost interchangably with very minor differences.  The database abstraction layer provides the following methods:
-
-- __queryArray (query, [options], callback)__
-  * Execute `query` using `options` if given and pass the result set as an array to `callback` function.
-  * `query` format for MongoDB is `{collectionName: {field: value}}`
-  * `query` format for PostgreSQL is `{text: 'select foo from bar where baz > $1 and bar < $2', values: [1,2]}`
-  * `callback` signature is `function (error, array)`
-  * Caution: all records will be loaded into memory.  Do not use for very large data sets.
-- __queryStream (query, [options], callback)__
-  * Execute `query` using `options` if given and pass a readable result stream to the `callback` function.
-  * `query` format for MongoDB is `{collectionName: {field: value}}`
-  * `query` format for PostgreSQL is `{text: 'select foo from bar where baz > $1 and bar < $2', values: [1,2]}`
-  * `callback` signature is `function (error, stream)`
-- __insert (insert, [options], callback)__
-  * Inserts `insert` into database using `options` if given.
-  * `insert` format is `{collectionOrTableName: {field: value}}`
-  * `callback` signature is `function (error, result)` where `result` is the number of affected records.
-- __update (query, update, [options], callback)__
-  * Applies `update` to records matching `query` using `options` if given.
-  * `query` format is `{collectionOrTableName: {field: value}}`
-  * `update` format is `{collectionOrTableName: {field: value}}`
-  * `callback` signature is `function (error, result)` where `result` is the number of affected records.
-- __upsert (query, update, [options], callback)__
-  * Applies `update` to records matching `query` using `options` if given.
-  * A new record will be inserted if no matching records are found.
-  * `query` format is `{collectionOrTableName: {field: value}}`
-  * `update` format is `{collectionOrTableName: {field: value}}`
-  * `callback` signature is `function (error, result)` where `result` is the number of affected records.
-
-
-### Operation Definitions
-
-The `db` parameter will be a `Database` instance configured for the specified database.  The `registry` parameter is a storage object that is shared among all functions for the entire operation. The operation functions are executed in this order:
-
-- __init (db, registry, callback)__ (_optional_)
-  * Executed once and must call `callback` method to proceed with operation.
-  * Useful for getting delta starting points or populating initial registry values.
-- __extract (db, registry, process, done)__
-  * Executed once, used to extract data from source database.
-  * `process` method should be called once for each record
-  * `done` method should be called after all records have been processed
-- __transform (db, registry, data)__ (_optional_)
-  * Executed once for each record emitted by `extract` functions.
-  * Used to reshape data before insertion into target database.
-  * Called synchronously, so just `return` the transformed data.
-- __load (db, registry, data, callback)__
-  * Executed once for each record emitted by `extract` and `transform` functions.
-  * Can be used to populate registry with aggregated or incremental data.
-  __interval (db, registry, callback)__ (_optional_)
-  * Executed at regular intervals after load function
-  * Useful for recording incremental progress in case of failures.
-- __exit (db, registry, callback)__ (_optional_)
-  * Executed once after all other functions have finished.
-  * Useful for cleaning up after operation, and for recording summary data.
-
-
-### Modules
-
-A Mongres module must export a JSON object like this:
-```
-module.exports = {
-
-  db: {
-
-    mongo: {
-      type: 'mongodb',
-      name: 'test',
-      host: 'localhost',
-      port: 27017,
-      user: 'username',
-      pass: 'password'
-    },
-
-    postgres: {
-      type: 'postgresql',
-      name: 'tnt2',
-      host: 'localhost',
-      port: 5432,
-      user: 'username',
-      pass: 'password'
-    }
-
-  },
+  db: require('./db.js'),
 
   op: {
 
@@ -297,5 +201,3 @@ module.exports = {
   }
 
 };
-
-```
